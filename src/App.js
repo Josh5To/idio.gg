6d0ed09893c23add7278c424ccd5671a736a717b
Josh5To <joshthej@icloud.com>
2022-12-28 21:32:11 -0500

    added bad favicon, removed not-used-yet tests

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <main>
           <p>nostr pubkey:</p>
-          <p>000</p>
+          <p>npub1833j3t9d0cc4lu36sj4l8gy5sf7hqkejmpk8tggnpmsqzn67z8nqewh2j8</p>
       </main>
     </div>
   );
